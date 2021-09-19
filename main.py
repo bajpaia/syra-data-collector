b4184733710b50482af85167009c1eec8ce4569a
bajpaia <animeshbajpai_sanjeev@yahoo.co.in>
2021-09-19 22:17:55 +0530

    test

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def load_sheet(COLUMNS = ['Date', 'Total Sales', '#Orders', 'Sessions', 'Retenti
 
 
 
-@sched.scheduled_job('cron', hour=16, minute=46)
+@sched.scheduled_job('cron', hour=16, minute=55)
 # @sched.scheduled_job('cron', hour=22, minute=15)
 def main():
     # options = Options()
